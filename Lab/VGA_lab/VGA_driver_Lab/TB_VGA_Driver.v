6b03ea0acc20d9c787086be21e93ffa5faaa8f67
Vince <58961531+andrealin355394@users.noreply.github.com>
2024-05-11 23:25:33 +0800

    Add files via upload

---
+++ Lab/VGA_lab/VGA_driver_Lab/TB_VGA_Driver.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/05/08 15:25:52
+// Design Name: 
+// Module Name: TB_VGA_Driver
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TB_VGA_Driver();
+
+
+reg     I_clk;
+reg     I_rst;
+
+initial
+begin
+   I_clk = 'b0;
+   I_rst = 'b1;
+   #50
+   I_rst = 'b0;
+end
+
+always #5 I_clk = ~I_clk;
+
+VGA_Driver u0(
+            .I_clk(I_clk),
+            .I_rst(I_rst)
+);
+
+
+endmodule
